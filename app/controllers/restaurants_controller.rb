d076c663b947f16295f6c4db3eda8d8e0888b2b5
Jordan Jalabert <jordan.jalabert@gmail.com>
2024-05-22 14:23:11 +0200

    exo 2

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,12 +1,25 @@
 class RestaurantsController < ApplicationController
-  def new
-  end
-
   def show
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
   def index
     @restaurant = Restaurant.all
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
